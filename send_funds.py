0a731b0d5e5c8f90c627549dd6dbe20225e46281
matteo-d <matteodrouin33@gmail.com>
2023-10-29 22:42:57 +0100

    Save get balance

--- send_funds.py
+++ send_funds.py
@@ -1,7 +1,6 @@
-# Script to send funds from futures to spot
-from config import BYBIT_API_KEY, BYBIT_API_SECRET, BYBIT_API_KEY_INTRA, BYBIT_API_SECRET_INTRA, BYBIT_API_KEY_INTRA2, BYBIT_API_SECRET_INTRA2, BYBIT_API_KEY_INTRA3, BYBIT_API_SECRET_INTRA3, BYBIT_API_KEY_CLASSIC, BYBIT_API_SECRET_CLASSIC, BYBIT_API_KEY_CLASSIC2, BYBIT_API_SECRET_CLASSIC2,BYBIT_API_KEY_CLASSIC3, BYBIT_API_SECRET_CLASSIC3, BYBIT_API_KEY_SWING, BYBIT_API_SECRET_SWING, BYBIT_API_KEY_SWING2, BYBIT_API_SECRET_SWING2, BYBIT_API_KEY_SWING3, BYBIT_API_SECRET_SWING3
+# Get Accounts Balance
+from config import BYBIT_API_KEY, BYBIT_API_SECRET, BYBIT_API_KEY_INTRA, BYBIT_API_SECRET_INTRA, BYBIT_API_KEY_INTRA2, BYBIT_API_SECRET_INTRA2, BYBIT_API_KEY_INTRA3, BYBIT_API_SECRET_INTRA3, BYBIT_API_KEY_CLASSIC, BYBIT_API_SECRET_CLASSIC, BYBIT_API_KEY_CLASSIC2, BYBIT_API_SECRET_CLASSIC2, BYBIT_API_KEY_CLASSIC3, BYBIT_API_SECRET_CLASSIC3, BYBIT_API_KEY_SWING, BYBIT_API_SECRET_SWING, BYBIT_API_KEY_SWING2, BYBIT_API_SECRET_SWING2, BYBIT_API_KEY_SWING3, BYBIT_API_SECRET_SWING3
 from pybit.unified_trading import HTTP
-
 
 session = HTTP(
     testnet=False,
@@ -54,18 +53,28 @@ sessionSwing3 = HTTP(
     api_secret=BYBIT_API_SECRET_SWING3,
 )
 
-MAIN = float(session.get_wallet_balance(accountType="SPOT")['result']['list'][0]['coin'][1]['walletBalance'])
-INTRA = float(sessionIntra.get_wallet_balance(accountType="CONTRACT")['result']['list'][0]['coin'][0]['equity'])
-CLASSIC = float(sessionClassic.get_wallet_balance(accountType="CONTRACT")['result']['list'][0]['coin'][0]['equity'])
-SWING = float(sessionSwing.get_wallet_balance(accountType="CONTRACT")['result']['list'][0]['coin'][0]['equity'])
-# Find a way to put equity = 0 if the wallet balance is empty for other wallets
-# For now solution is to always have at least one dollar on each subbacount
-INTRA2 = float(sessionIntra2.get_wallet_balance(accountType="CONTRACT")['result']['list'][0]['coin'][0]['equity'])
-CLASSIC2 = float(sessionClassic2.get_wallet_balance(accountType="CONTRACT")['result']['list'][0]['coin'][0]['equity'])
-SWING2 = float(sessionSwing3.get_wallet_balance(accountType="CONTRACT")['result']['list'][0]['coin'][0]['equity'])
+MAIN = float(session.get_wallet_balance(accountType="SPOT")[
+             'result']['list'][0]['coin'][1]['walletBalance'])
+INTRA = float(sessionIntra.get_wallet_balance(accountType="CONTRACT")[
+              'result']['list'][0]['coin'][0]['equity'])
+CLASSIC = float(sessionClassic.get_wallet_balance(accountType="CONTRACT")[
+                'result']['list'][0]['coin'][0]['equity'])
+SWING = float(sessionSwing.get_wallet_balance(accountType="CONTRACT")[
+              'result']['list'][0]['coin'][0]['equity'])
 
-INTRA3 = float(sessionIntra3.get_wallet_balance(accountType="CONTRACT")['result']['list'][0]['coin'][0]['equity'])
-CLASSIC3 = float(sessionClassic3.get_wallet_balance(accountType="CONTRACT")['result']['list'][0]['coin'][0]['equity'])
-SWING3 = float(sessionSwing3.get_wallet_balance(accountType="CONTRACT")['result']['list'][0]['coin'][0]['equity'])
+INTRA2 = float(sessionIntra2.get_wallet_balance(accountType="CONTRACT")[
+               'result']['list'][0]['coin'][0]['equity'])
+CLASSIC2 = float(sessionClassic2.get_wallet_balance(accountType="CONTRACT")[
+                 'result']['list'][0]['coin'][0]['equity'])
+SWING2 = float(sessionSwing3.get_wallet_balance(accountType="CONTRACT")[
+               'result']['list'][0]['coin'][0]['equity'])
 
-print(MAIN, INTRA, CLASSIC, SWING, INTRA2, CLASSIC2, SWING2, INTRA3, CLASSIC3, SWING3)
+INTRA3 = float(sessionIntra3.get_wallet_balance(accountType="CONTRACT")[
+               'result']['list'][0]['coin'][0]['equity'])
+CLASSIC3 = float(sessionClassic3.get_wallet_balance(accountType="CONTRACT")[
+                 'result']['list'][0]['coin'][0]['equity'])
+SWING3 = float(sessionSwing3.get_wallet_balance(accountType="CONTRACT")[
+               'result']['list'][0]['coin'][0]['equity'])
+
+print(MAIN, INTRA, CLASSIC, SWING, INTRA2,
+      CLASSIC2, SWING2, INTRA3, CLASSIC3, SWING3)
