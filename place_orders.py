5aaecaa0335b825b545bad1d7648453d855c8974
matteo-d <matteodrouin33@gmail.com>
2023-11-04 12:54:11 +0100

    Save

---
+++ place_orders.py
@@ -0,0 +1,10 @@
+# Import prices data from scan folder
+# Get Available funds on main SPOT account
+# 50% are for Alts, 25% BTC, 25% ETH
+# SELECT ALTS
+    # list alts
+# Get value of USDT That I can put for each position
+# Get minimum quantity that I can buy for each order 
+# Calculate minimum quantity * price
+# If inferior to Value of USDT that I can put for each position
+    # Place orders
