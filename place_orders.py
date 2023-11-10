f8d96440cc7fc510a8d42b0e02943cbc88a6da5d
matteo-d <matteodrouin33@gmail.com>
2023-11-10 18:17:29 +0100

    Working on place orders

--- place_orders.py
+++ place_orders.py
@@ -1,10 +1,70 @@
-# Import prices data from scan folder
-# Get Available funds on main SPOT account
-# 50% are for Alts, 25% BTC, 25% ETH
-# SELECT ALTS
-    # list alts
-# Get value of USDT That I can put for each position
-# Get minimum quantity that I can buy for each order 
-# Calculate minimum quantity * price
-# If inferior to Value of USDT that I can put for each position
-    # Place orders
+from config import BYBIT_API_KEY, BYBIT_API_SECRET
+from pybit.unified_trading import HTTP
+import uuid
+import math
+# CREATE SESSION TO USE USE API V5
+session = HTTP(
+    testnet=False,
+    api_key=BYBIT_API_KEY,
+    api_secret=BYBIT_API_SECRET,
+)
+
+# PRICE LEVELS INTERESTING
+levels = [
+    [3806.0],
+    [6307.0],
+    [7925.5],
+    [10316.5],
+    [12831.0],
+    [16016.0],
+    [17770.0],
+    [18235.0],
+    [20093.0],
+    [21199.5],
+    [22914.0],
+    [23318.0],
+    [24350.5],
+    [26252.0],
+    [27953.5],
+    [28396.5],
+    [29168.0],
+    [29573.5],
+    [30021.0],
+    [32902.0],
+    [34760.5],
+    [36044.0],
+    [42960.5],
+    [46542.0],
+    [50733.0],
+    [57134.0],
+    [58876.0],
+    [61477.5]
+]
+
+print(type(levels))
+# MINIMUM POSITION SIZE IN COIN DENOMINATION
+minimum_position = 0.001
+
+# AVAILABLE BALANCE IN USDT
+MAIN_BALANCE = float(session.get_wallet_balance(accountType="SPOT")[
+    'result']['list'][0]['coin'][1]['walletBalance'])
+print("TOTAL ON MAIN ACCOUNT", MAIN_BALANCE, "$ \n\n")
+
+# GET VALUE IN USDT OF THE DESIRED POSITION IN USDT
+allocation = (int(MAIN_BALANCE) * 0.2) - 1
+print(allocation)
+
+# GET CURRENT PRICE OF THE COIN
+coin_infos = session.get_tickers(
+    category="spot",
+    symbol="BTCUSDT",
+)
+current_price = math.floor(float(coin_infos['result']['list'][0]['usdIndexPrice']))
+print(current_price)
+
+# FIND INDEX OF THE PRICE CLOSEST TO THE CURRENT PRICE OF THE COIN
+def find_closest_index(arr, x):
+    closest_index = min(range(len(arr)), key=lambda i: abs(arr[i] - x))
+    return closest_index
+closest_index = find_closest_index(levels, current_price)
+print(f"The index of the element closest to {current_price} is: {closest_index}")
--- rebalance.py
+++ rebalance.py
@@ -1,6 +1,7 @@
-from config import BYBIT_API_KEY, BYBIT_API_SECRET
+"""from config import BYBIT_API_KEY, BYBIT_API_SECRET
 from config import MAIN_ID,INTRA_ID,CLASSIC_ID,SWING_ID,INTRA2_ID,CLASSIC2_ID,SWING2_ID,INTRA3_ID,CLASSIC3_ID,SWING3_ID
 from get_transferable_funds import INTRA_TRANSFERABLE, CLASSIC_TRANSFERABLE, SWING_TRANSFERABLE, INTRA2_TRANSFERABLE, CLASSIC2_TRANSFERABLE, SWING2_TRANSFERABLE, INTRA3_TRANSFERABLE, CLASSIC3_TRANSFERABLE, SWING3_TRANSFERABLE  
+from get_balances import SHOULD_BE_ON_INTRAS, SHOULD_BE_ON_CLASSICS, SHOULD_BE_ON_SWINGS
 import math
 from pybit.unified_trading import HTTP
 import uuid
@@ -10,42 +11,33 @@ session = HTTP(
     api_key=BYBIT_API_KEY,
     api_secret=BYBIT_API_SECRET,
 )
-# SENDING ALL AVAILABLE FUNDS ON SUBBACOUNTS TO MAIN ACCOUNT
+
 from_member_ids = [INTRA_ID, CLASSIC_ID, SWING_ID, INTRA2_ID, CLASSIC2_ID, SWING2_ID, INTRA3_ID, CLASSIC3_ID, SWING3_ID]
 amounts = [INTRA_TRANSFERABLE['result']['balance']['transferBalance'], CLASSIC_TRANSFERABLE['result']['balance']['transferBalance'], SWING_TRANSFERABLE['result']['balance']['transferBalance'], INTRA2_TRANSFERABLE['result']['balance']['transferBalance'], CLASSIC2_TRANSFERABLE['result']['balance']['transferBalance'], SWING2_TRANSFERABLE['result']['balance']['transferBalance'], INTRA3_TRANSFERABLE['result']['balance']['transferBalance'], CLASSIC3_TRANSFERABLE['result']['balance']['transferBalance'], SWING3_TRANSFERABLE['result']['balance']['transferBalance']]
 
-def fund_spot_account(session, from_member_ids, amounts):
-    if len(from_member_ids) != len(amounts):
-        raise ValueError("Length of from_member_ids and amounts lists must be the same.")
+to_intras = [INTRA_ID, INTRA2_ID, INTRA3_ID]
 
+def fund_intra_accounts(session, to_intras):
     coin = "USDT"
-    from_account_type = "CONTRACT"
-    to_account_type = "SPOT"
-
-    results = []
-
-    for from_member_id, amount in zip(from_member_ids, amounts):
+    from_account_type = "SPOT"
+    to_account_type = "CONTRACT"
+    results_intras = []
+    print(SHOULD_BE_ON_INTRAS)
+    for to_intra in zip(to_intras):
         transfer_id = str(uuid.uuid4())
-        print(amount)
         ajusted_amount = math.floor(float(amount)) -1
         if ajusted_amount > 1 :
             result = session.create_universal_transfer(
                 transferId=transfer_id,
                 coin=coin,
-                amount=str(ajusted_amount),
-                fromMemberId=from_member_id,
-                toMemberId=MAIN_ID, 
+                amount=str(int(SHOULD_BE_ON_INTRAS)),
+                fromMemberId=MAIN_ID,
+                toMemberId=to_intra, 
                 fromAccountType=from_account_type,
                 toAccountType=to_account_type
             )
-            results.append(result)
-        else : 
-            print("Already empty")
-    return results
+            results_intras.append(result)
+        return results_intras
 
-
-print(amounts)
-results = fund_spot_account(session, from_member_ids, amounts)
-
-for result in results:
-    print(result)
+results_intras = fund_intra_accounts(session, to_intras)
+"""
